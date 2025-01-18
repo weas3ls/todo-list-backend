fee3c7f44cabce8ed27a5980c217e5eaeb6883d3
weas3ls <shabaneh.ahmad@gmail.com>
2025-01-17 16:10:13 -0800

    setup initial express

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello from Express with TypeScript!");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
