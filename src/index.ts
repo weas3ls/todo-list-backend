e53266d7382d79ea2e27c3dd13bca3830178ebec
weas3ls <shabaneh.ahmad@gmail.com>
2025-01-18 17:35:04 -0800

    fix cors issue

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express, { Application } from "express";
 
 import { routes } from "./routes";
@@ -7,6 +8,18 @@ const port = process.env.PORT;
 
 //* Middleware to parse JSON
 app.use(express.json());
+
+//* Middleware to enable CORS from the client-side http://localhost:3001
+app.use(
+    cors({
+        //? Allow only this origin
+        origin: "http://localhost:3001",
+        //? Allow these HTTP methods
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        //? Allow these headers
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+);
 
 app.use("/", routes);
 
